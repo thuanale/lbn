3efdb602ce576caa052bc159c849b27fa13970c6
thuanale <thuanale@gmail.com>
2021-05-14 12:53:05 +0700

    v3

--- 3-Automate password/sqlite/query_db.py
+++ 3-Automate password/sqlite/query_db.py
@@ -3,14 +3,20 @@
 import sqlite3
 import sys
 
-email = 'V.dang@linkbynet.com'#sys.argv[1]
+email = sys.argv[1]
 
 conn = sqlite3.connect('D:/2021/1-Code/LBN/3-Automate password/sqlite/lbn_as400pw.db')
 cur = conn.cursor()
 cur.execute("SELECT * FROM USERS WHERE email LIKE :email",{"email": email})
 row = cur.fetchone()
 if isinstance(row,tuple):
-    print(row)
+    cur.execute("SELECT server_id FROM SERVERS WHERE server_id IN \
+                 (SELECT server_id FROM USERS_SERVERS \
+                  WHERE user_id = (SELECT user_id FROM USERS WHERE email LIKE :email))",{"email": email})
+    servers = cur.fetchall() 
+    print(servers)    
+    for server in servers:
+        print(server)
 else:
     print("YOUR EMAIL IS NOT FOUND!")
 conn.close()
