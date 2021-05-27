38a6ceb95c420b0810bf552928d62049e7c5bcfe
thuanale <thuanale@gmail.com>
2021-05-27 16:44:00 +0700

    vim 1.0

--- 3-Automate password/sqlite/query_db.py
+++ 3-Automate password/sqlite/query_db.py
@@ -4,19 +4,18 @@ import sqlite3
 import sys
 
 email = sys.argv[1]
+as400_id = sys.argv[2]
 
-conn = sqlite3.connect('D:/2021/1-Code/LBN/3-Automate password/sqlite/lbn_as400pw.db')
+conn = sqlite3.connect('./lbn_as400pw.db')
 cur = conn.cursor()
-cur.execute("SELECT * FROM USERS WHERE email LIKE :email",{"email": email})
+cur.execute("SELECT * FROM USERS WHERE email LIKE :email AND as400_id LIKE :as400_id",{"email": email,"as400_id": as400_id})
 row = cur.fetchone()
 if isinstance(row,tuple):
-    cur.execute("SELECT server_id FROM SERVERS WHERE server_id IN \
+    cur.execute("SELECT server_id, server_name FROM SERVERS WHERE server_id IN \
                  (SELECT server_id FROM USERS_SERVERS \
-                  WHERE user_id = (SELECT user_id FROM USERS WHERE email LIKE :email))",{"email": email})
+                  WHERE user_id = (SELECT user_id FROM USERS WHERE email LIKE :email AND as400_id LIKE :as400_id))",{"email": email,"as400_id": as400_id})
     servers = cur.fetchall() 
     print(servers)    
-    for server in servers:
-        print(server)
 else:
     print("YOUR EMAIL IS NOT FOUND!")
 conn.close()
