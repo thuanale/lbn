d71b4e4ca6ee586fbd18d55934e0e87606023464
thuanale <thuanale@gmail.com>
2021-06-28 01:56:39 +0700

    v2.0

--- 1-Tape_Movement/Crowntk.py
+++ 1-Tape_Movement/Crowntk.py
@@ -2,8 +2,9 @@
 
 from tkinter import *
 from tkinter import Tk, ttk
+from login_web import *
 
-class App(ttk.Frame):
+class App(ttk.Frame):  
   def __init__(self,master=None):
     self.master = master
     super().__init__(self.master, padding='3 3 12 12')
@@ -11,42 +12,51 @@ class App(ttk.Frame):
     self.grid(column=0, row=0, sticky=(N, W, E, S))
     self.create_widgets()
     self.set_labels()
-
+    self.browser = None
+    
 
   def create_widgets(self):
-    user = StringVar()
-    user_entry = Entry(self, width=20, textvariable=user)
-    user_entry.grid(column=2, row=1, sticky=(W, E))
-    user_entry.focus()
+    self.uid = StringVar()
+    uid_entry = Entry(self, width=20, textvariable=self.uid)
+    uid_entry.grid(column=2, row=1, sticky=(W, E))
+    uid_entry.focus()
 
-    password = StringVar()
-    Entry(self, width=20, textvariable=password, show='*').grid(column=2, row=2,
+    self.pw = StringVar()
+    Entry(self, width=20, textvariable=self.pw, show='*').grid(column=2, row=2,
     sticky=(W, E))
 
-    ttk.Button(self, text="Start", command=self.login).grid(column=1, row=3,
+    ttk.Button(self, text="Start", command=self.start).grid(column=1, row=3,
     sticky=(W, E))
-    ttk.Button(self, text="Again", command=self.rerun).grid(column=3, row=3,
+    ttk.Button(self, text="Run", command=self.run).grid(column=3, row=3,
     sticky=(W, E))
-    ttk.Button(self, text="Stop", command=self.logout).grid(column=2, row=4,
+    ttk.Button(self, text="Stop", command=self.stop).grid(column=2, row=4,
     sticky=(W, E))
   
   def set_labels(self):
     self.master.title("Crown Check Tk")
     ttk.Label(self, text="ID").grid(column=1, row=1, sticky=(E))
-    ttk.Label(self, text="Password").grid(column=1, row=2, sticky=(E))
+    ttk.Label(self, text="pw").grid(column=1, row=2, sticky=(E))
     
     for child in self.winfo_children(): 
       child.grid_configure(padx=5, pady=5)
 
-  def login(self):
-    pass
+  def start(self):
+    self.browser = CrownRM()
+    self.browser.setup()
+    UID = str(self.uid.get())
+    PW = str(self.pw.get())
+    self.browser.login(UID, PW)
+    self.browser.go_to_search()
 
-  def rerun(self):
-    pass
 
-  def logout(self):
-    pass
+  def run(self):
+    self.browser.prepare_search()
+    self.browser.load_tape(FILE)
+    self.browser.check_tape()
+    
 
-root = Tk()
-app = App(master=root)
-app.mainloop()
+  def stop(self):
+    if self.browser:
+      self.browser.tear_down()
+    self.destroy()
+    exit()
