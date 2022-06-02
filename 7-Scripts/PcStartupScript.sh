c2546bf7ca17c3b873c2fc0df08e531d9e02e3bc
thuanale <thuanale@gmail.com>
2022-06-02 19:16:13 +0700

    220602 Add condition

--- 7-Scripts/PcStartupScript.sh
+++ 7-Scripts/PcStartupScript.sh
@@ -2,18 +2,18 @@
 
 AgentPid="$HOME/AGENT.PID"
 
-if (( $(grep -c ssh-agent <(ps aux)) >=2 and -s $AgentPid )); then
+if (( $(grep -c ssh-agent <(ps aux)) >= 2 )) && [[ -s $AgentPid ]]; then
+  echo "+++ LOAD ENVIRONMENT PARAMETER+++"
   source $AgentPid
 else
+  echo "+++ START SSH-AGENT +++"
   eval $(ssh-agent -s)
   env | grep -i ssh > $AgentPid
 fi
-
 echo "+++ LOAD KEY FOR GIT +++"
 ssh-add $HOME/.ssh/id_ed25519
 echo "+++ LOAD KEY FOR DFS BASTION HOSTS +++"
 ssh-add $HOME/.ssh/ssh-pri
-
 echo "+++ Local port forwarding for AS400 Monitoring webapp. +++"
 ssh -L 8989:10.242.0.38:3389 t.le@172.21.198.39 -N -f
 echo "+++ Dynamic port forwarding for other Application at DFS. +++"
