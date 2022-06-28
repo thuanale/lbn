a0c69484f7ecbdc9aa835c2c4dc163c5734e978c
thuanale <thuanale@gmail.com>
2022-06-28 13:59:51 +0700

    220628 Update ssh script

--- 7-Scripts/PcStartupScript.sh
+++ 7-Scripts/PcStartupScript.sh
@@ -15,7 +15,7 @@ ssh-add $HOME/.ssh/id_ed25519
 echo "+++ LOAD KEY FOR DFS BASTION HOSTS +++"
 ssh-add $HOME/.ssh/ssh-pri
 echo "+++ Local port forwarding for AS400 Monitoring webapp. +++"
-ssh -L 8989:10.242.0.38:3389 t.le@172.21.198.39 -N -f
+ssh -o ServerAliveInterval=300 -L 8989:10.242.0.38:3389 t.le@172.21.198.39 -N -f
 echo "+++ Dynamic port forwarding for other Application at DFS. +++"
 ssh -C -D 8888 t.le@172.21.198.39 -N -f
 echo "END."
