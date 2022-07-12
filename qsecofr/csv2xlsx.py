9d66f4928f877fbf6460b0d13e9d62b96fa05b8c
thuanale <thuanale@gmail.com>
2022-07-13 00:18:58 +0700

    220713 Update create report function

--- qsecofr/csv2xlsx.py
+++ qsecofr/csv2xlsx.py
@@ -1,20 +1,48 @@
 #!/usr/bin/env python3
 import csv
-from openpyxl import Workbook
-from os.path import basename,splitext
+from os.path import basename
+from datetime import date
 import glob
-import zipfile
-from io import TextIOWrapper
+import pandas as pd
 
-directory = "/home/thomas-le/Desktop/xxxn"
-for QsecorfFile in glob.glob("{0}/AS400_ATSECOFR_*.zip".format(directory)):
-  wb = Workbook(write_only = True)
-  with zipfile.ZipFile(QsecorfFile) as zf:
-    for csvfile in zf.namelist():
-      with zf.open(csvfile) as csvf:
-        ws = wb.create_sheet(title = csvfile.split('_')[0])
-        reader = csv.reader(TextIOWrapper(csvf,encoding='utf-8',newline=''))
-        for row in reader:
-          ws.append(row)
-  wb.save(filename="{0}.xlsx".format(splitext(basename(QsecorfFile))[0]))
+directory = "/home/thomas-le/Desktop/xxxn/qsecofr"
+newDir = "{0}/new".format(directory)
+oldDir = "{0}/old".format(directory)
+monthlyFile = "{0}/Qsecofr_Audit_Review_{1}.xlsx".format(directory, date.today().strftime("%b_%Y"))
+servers = ["CAM","CATS","CLUX","DFSNZ1","FRANCE","GDC","GVA","HKGA","HNL","INDO","ITL","KOREA","LAX1","MIDEAST","MIDPAC","PAD1","PAX1","PRC","SAP2PFS", "SFO2", "SFO", "SIN2", "SYD"]
 
+def checkFiles():
+  missedFiles = []
+
+  for server in servers:
+    if glob.glob("{0}/*{1}*".format(newDir,server)) == []:
+      missedFiles.append(server)
+
+  if missedFiles == []:
+    return True
+  else:
+    print("Below file(s) are missed. Please check w/ L2.")
+    for file in missedFiles:
+      print(file)
+    return False
+
+def createReport():
+  newFiles = sorted(glob.glob("./{0}/*.csv".format(newDir)))
+  oldFiles = sorted(glob.glob("./{0}/*.csv".format(oldDir)))
+  for newFile, oldFile in zip(newFiles, oldFiles):
+    old = pd.read_csv(oldFile,encoding='unicode_escape',header=None).iloc[:,3:]
+    new = pd.read_csv(newFile,encoding='unicode_escape',header=None).iloc[:,3:]
+    result = pd.concat([new,old]).drop_duplicates(keep=False)
+    if not result.empty:
+      results = pd.concat([results,result])
+
+def createExcel():
+  with pd.ExcelWriter(monthlyFile) as writer:
+    for csvf in sorted(glob.glob("{0}/*".format(newDir))):
+      sheetname = basename(csvf).split('_')[0]
+      reader = pd.read_csv(csvf,encoding="unicode_escape",header = None)
+      reader.to_excel(writer,index=False,header=False,sheet_name=sheetname)
+
+if checkFiles():
+  createReport()
+  createExcel()
